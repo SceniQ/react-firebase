a5966b4886d1c7d6a07241ad4345b7b2f94c9522
SceniQ <tassagaai19@gmail.com>
2025-08-16 22:10:34 +0200

    outputting lists

--- chapter4/first-react-app/src/App.js
+++ chapter4/first-react-app/src/App.js
@@ -5,6 +5,11 @@ import { useState } from 'react';
 
 function App() {
   const [name, setName] = useState('Tebza A')
+  const [events, setEvents] = useState([
+    {title: "Tebza A's first React lesson",id:1},
+    {title: "Tebza A's first React lesson",id:2},
+    {title: "Tebza A's first React lesson",id:3}
+  ])
   const handleClick = () =>{
     setName('Mrembola')
     console.log(name)
@@ -12,7 +17,12 @@ function App() {
   return (
     <div className="App">
       <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Show name</button>    
+      <button onClick={handleClick}>Show name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}    
     </div>
   );
 }
