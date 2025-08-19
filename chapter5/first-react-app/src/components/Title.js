949ed1265508976b3d678c61b9da2640a5996673
SceniQ <tassagaai19@gmail.com>
2025-08-19 20:26:54 +0200

    multiple components

--- chapter5/first-react-app/src/components/Title.js
+++ chapter5/first-react-app/src/components/Title.js
@@ -1,6 +1,6 @@
 export default function Title(){
     return(
-        <div className="title">
+        <div>
             <h1 className="title">Tebza A's react attempt events</h1>
             <br/>
             <h2 className="subtitle"> All the latest attempts in building a React app</h2>
