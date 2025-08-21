3a53cd7018d0b3880134fcc9d61e411a30d1c293
SceniQ <tassagaai19@gmail.com>
2025-08-21 21:20:37 +0200

    dynamic classes

--- chapter6/first-react-app/src/components/Modal.js
+++ chapter6/first-react-app/src/components/Modal.js
@@ -9,7 +9,7 @@ export default function Modal({children, handleClose, isSalesModal}) {
           borderColor: isSalesModal ? "#ff4500" :"#555",
           textAlign: "center"}}>
             {children}
-            <button onClick={handleClose}>close</button>
+            <button onClick={handleClose} className={isSalesModal? "sales-btn" : ""}>close</button>
         </div>
       </div>
   ), document.body)
