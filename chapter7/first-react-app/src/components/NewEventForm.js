414ac6f39620dfc3039080cd65a3731c340852d5
SceniQ <tassagaai19@gmail.com>
2025-08-25 16:04:27 +0200

    contolled inputs

--- chapter7/first-react-app/src/components/NewEventForm.js
+++ chapter7/first-react-app/src/components/NewEventForm.js
@@ -9,14 +9,15 @@ export default function NewEventForm() {
     <form className='new-event-form'>
         <label>
             <span>Event title:</span>
-            <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
         </label>
         <label>
             <span>Event date:</span>
-            <input type="date" onChange={(e) => setDate(e.target.value)} />
+            <input type="date" onChange={(e) => setDate(e.target.value)} value={date}/>
         </label>
         <button>Submit</button>
         <p>Added title: {title}, Date: {date}</p>
+        <a href="#" style={{color:"#555"}} onClick={() => {setTitle('');setDate('')}}>reset form</a>
     </form>
   )
 }
