1be36bf88ad7fe4cb58055f1b712eef1142920c5
SceniQ <tassagaai19@gmail.com>
2025-08-25 12:15:42 +0200

    css modules

--- chapter6/first-react-app/src/components/EventList.js
+++ chapter6/first-react-app/src/components/EventList.js
@@ -1,13 +1,14 @@
 import './Modal.css';
 import React from 'react';
+import styles from './EventList.module.css';
 export default function EventList({events, handleClick}) {
     return (
-        <div className="eventList">
+        <div>
             {events.map((event, index) => (
-                    <React.Fragment key={event.id}>
+                    <div className={styles.card} key={event.id}>
                     <h2>{index} - {event.title}</h2>
                     <button onClick={() => handleClick(event.id)}>Delete item</button>
-                    </React.Fragment>
+                    </div>
                 ))}
         </div>
     )
