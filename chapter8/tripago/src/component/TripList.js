c7b93468b4f85c8477650e46c00389c36db64d17
SceniQ <tassagaai19@gmail.com>
2025-08-27 11:33:22 +0200

    fetching data with useEffect

--- chapter8/tripago/src/component/TripList.js
+++ chapter8/tripago/src/component/TripList.js
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function TripList() {
     const [trips, setTrips] = useState([])
 
+    useEffect(() => {
+        fetch('http://localhost:3000/trips')
+            .then(respone => respone.json())
+            .then(json => setTrips(json))
+    }, [])
+
     console.log(trips)
-    fetch('http://localhost:3000/trips')
-    .then(respone => respone.json())
-    .then(json => setTrips(json))
+
+    
+
+
   return (
     <div>
       <h2>Trip List:</h2>
+      {trips.map(trip => (
+        <ul>
+            <li key={trip.id}>{trip.title} - {trip.price}</li>
+        </ul>
+      ))}
     </div>
   )
 }
