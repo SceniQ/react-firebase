10e3c7097f63a68cf831e766be78fc43cceb2230
SceniQ <tassagaai19@gmail.com>
2025-08-28 11:03:07 +0200

    useEffect dependency array

--- chapter8/tripago/src/component/TripList.js
+++ chapter8/tripago/src/component/TripList.js
@@ -3,14 +3,27 @@ import './styles/triplist.css'
 
 export default function TripList() {
     const [trips, setTrips] = useState([])
+    const [url,setUrl] = useState('http://localhost:3000/trips')
+    const [tripLocationUrl] = useState('http://localhost:3000/trips')
+    const [tripLocations, setTriplocation] = useState([])
+
 
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
+        fetch(tripLocationUrl)
+            .then(respone => respone.json())
+            .then(json => setTriplocation(json))
+    }, [tripLocationUrl]) 
+
+    useEffect(() => {
+        fetch(url)
             .then(respone => respone.json())
             .then(json => setTrips(json))
-    }, [])
+    }, [url])
+
+    
 
     console.log(trips)
+   
 
     
 
@@ -18,6 +31,15 @@ export default function TripList() {
   return (
     <div className="trip-list">
       <h2>Trip List:</h2>
+      <div className="filter">
+        <p>filter by location:</p>
+        <select onChange={(e) => "All" === e.target.value ? setUrl('http://localhost:3000/trips') : setUrl('http://localhost:3000/trips?location='.concat(e.target.value))}>
+            {tripLocations.map(trip =>(
+                <option value={trip.location}>{trip.location}</option>
+            ))}
+            <option value="All"> All</option>
+        </select>
+      </div>
       {trips.map(trip => (
         <ul>
             <li key={trip.id}>
