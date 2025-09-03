3d9b0808faab603f47cbe26de622c1b0238842f2
SceniQ <tassagaai19@gmail.com>
2025-09-03 21:11:38 +0200

    card component

--- chapter9/magic-match/src/component/CardGrid.js
+++ chapter9/magic-match/src/component/CardGrid.js
@@ -1,15 +1,11 @@
-import styles from '../styles/CardGrid.css'
+import '../styles/CardGrid.css'
+import SingleCard from './SingleCard'
 
 export default function CardGrid({gameCards}) {
   return (
     <div className="card-grid">
       {gameCards.map((card) => (
-        <div className="card" key={card.id}>
-            <div>
-                <img className="front" src={card.src} alt="card-front" />
-                <img className="back" src="/img/cover.png" alt="card-back" />
-            </div>
-        </div>
+        <SingleCard key={card.id} card={card}/>
       ))}
     </div>
   )
---
+++ chapter9/magic-match/src/component/SingleCard.js
@@ -0,0 +1,13 @@
+import '../styles/SingleCard.css'
+export default function SingleCard({card}) {
+  return (
+    <div className="single-card">
+      <div className="card">
+            <div>
+                <img className="front" src={card.src} alt="card-front" />
+                <img className="back" src="/img/cover.png" alt="card-back" />
+            </div>
+        </div>
+    </div>
+  )
+}
