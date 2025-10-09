71ed076ec3f97d62dc2caeac85fa7d31f4ab602b
SceniQ <tassagaai19@gmail.com>
2025-10-09 15:22:48 +0200

    routes

--- chapter10/multipage-site/src/App.js
+++ chapter10/multipage-site/src/App.js
@@ -1,5 +1,8 @@
 import './App.css'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
 
 function App() {
   return (
@@ -8,6 +11,11 @@ function App() {
       <nav>
         <h1>My Articles</h1>
       </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       </BrowserRouter>
     </div>
   );
