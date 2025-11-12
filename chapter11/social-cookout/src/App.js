874d418f9c8bae95ab3735d2c2c7506df8efad8a
SceniQ <tassagaai19@gmail.com>
2025-11-12 22:05:54 +0200

    project pages

--- chapter11/social-cookout/src/App.js
+++ chapter11/social-cookout/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import RecipeDetails from './pages/recipe/RecipeDetails'
+import SearchResults from './pages/search/SearchResults'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/create' element={<Create/>}/>
+          <Route exact path='/search' element={<SearchResults/>}/>
+          <Route exact path='/recipe' element={<RecipeDetails/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
