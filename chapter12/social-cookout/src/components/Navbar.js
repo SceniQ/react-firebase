240899921c4b4553e692ffafc1531ea65dd4df69
SceniQ <tassagaai19@gmail.com>
2025-12-02 20:16:32 +0200

    context api

--- chapter12/social-cookout/src/components/Navbar.js
+++ chapter12/social-cookout/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
+import { useContext } from 'react'
+import {ThemeContext} from '../context/ThemeContext'
 
 export default function Navbar() {
+  const {color} = useContext(ThemeContext)
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{background: color}}>
       <nav>
         <Link to='/' className='brand'><h1>Social-Cookout</h1> </Link>
         <SearchBar/>
